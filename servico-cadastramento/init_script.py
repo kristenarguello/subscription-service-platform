39db81210b3273e1c07666fe9b4f7e5fce10f4f2
Kristen <kristenarguello20@gmail.com>
2024-06-18 20:48:11 -0300

    Add real data to script

--- servico-cadastramento/init_script.py
+++ servico-cadastramento/init_script.py
@@ -1,22 +1,51 @@
-from pymongo import MongoClient
+import random
 from datetime import datetime
 
-client = MongoClient("localhost", 27017)
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
 
 db = client["cadastro_geral"]
 
+
 # pelo menos 10 clientes
 collection = db["clientes"]
+index_name = collection.create_index([("email", pymongo.ASCENDING)], unique=True)
 clientes = []
+people_names = [
+    "James",
+    "Maria",
+    "Robert",
+    "Sophia",
+    "Michael",
+    "Linda",
+    "William",
+    "Elizabeth",
+    "David",
+    "Jennifer",
+]
 for i in range(10):
-    clientes.append({"codigo": 1, "nome": "", "email": ""})
+    clientes.append(
+        {
+            "codigo": random.randint(10000, 99999),
+            "nome": people_names[i],
+            "email": f"{people_names[i].lower()}@email.com",
+        }
+    )
     collection.insert_one(clientes[i])
 
 # 5 aplicativos diferentes
 collection = db["aplicativos"]
 aplicativos = []
+app_names = ["QuickQuest", "FitFleet", "MindMerge", "ChoreChampion", "TravelTrove"]
 for i in range(5):
-    aplicativos.append({"codigo": 1, "nome": "", "custo_mensal": 0.0})
+    aplicativos.append(
+        {
+            "codigo": random.randint(10000, 99999),
+            "nome": app_names[i],
+            "custo_mensal": random.uniform(10.0, 25.0),
+        }
+    )
     collection.insert_one(aplicativos[i])
 
 # pelo menos 5 assinaturas diferentes
@@ -25,7 +54,7 @@ assinaturas = []
 for i in range(5):
     assinaturas.append(
         {
-            "codigo": 1,
+            "codigo": random.randint(10000, 99999),
             "cod_aplicativo": aplicativos[i]["codigo"],
             "cod_cliente": clientes[i]["codigo"],
             "inicio_vigencia": datetime.now(),
