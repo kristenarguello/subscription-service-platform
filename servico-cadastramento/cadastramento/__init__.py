04c54af88a1f219ba814dae1a70ec5d0f82b2b4b
Kristen <kristenarguello20@gmail.com>
2024-06-18 22:25:59 -0300

    Init all of the routes

--- servico-cadastramento/cadastramento/__init__.py
+++ servico-cadastramento/cadastramento/__init__.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter, FastAPI
 
 from .health.health import router as health_router
 from .aplicativos.routes import router as aplicativos_router
+from .clientes.routes import router as clientes_router
+from .assinaturas.routes import router as assinaturas_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Cadastramento")
     app.include_router(health_router, prefix="/health")
+    app.include_router(clientes_router)
     app.include_router(aplicativos_router)
+    app.include_router(assinaturas_router)
     return app
