14445cbef2a700cfe0f9549ff25c229074aaf54d
Kristen <kristenarguello20@gmail.com>
2024-06-18 22:26:52 -0300

    Add all logic for the clientes endpoints

--- servico-cadastramento/cadastramento/clientes/controllers.py
+++ servico-cadastramento/cadastramento/clientes/controllers.py
@@ -1,10 +1,27 @@
+from datetime import datetime
+from typing import Literal
+import pymongo
 from .schemas import GetClientes
 from ..assinaturas.schemas import GetAssinaturas
+from ..assinaturas.controllers import parse_assinaturas_from_db
 
 
 def get_clientes() -> list[GetClientes]:
-    raise NotImplementedError
+    client = pymongo.MongoClient("localhost", 27017)
+    db = client["cadastro_geral"]
+    collection = db["clientes"]
+
+    clientes_cursor = collection.find(projection={"_id": 0})
+    clientes_cursor = list(clientes_cursor)
+    return clientes_cursor
 
 
 def get_assinaturas_cliente(codigo_cli: int) -> list[GetAssinaturas]:
-    raise NotImplementedError
+    client = pymongo.MongoClient("localhost", 27017)
+    db = client["cadastro_geral"]
+    collection = db["assinaturas"]
+
+    assinaturas_cursor = collection.find(
+        {"cod_cliente": codigo_cli}, projection={"_id": 0}
+    )
+    return parse_assinaturas_from_db(list(assinaturas_cursor))
