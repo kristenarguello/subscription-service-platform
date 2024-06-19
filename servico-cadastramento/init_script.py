1e194dbadf56d1aa74cd38a8d9f81c7942b465ba
Kristen <kristenarguello20@gmail.com>
2024-06-18 22:25:20 -0300

    Finish init script

--- servico-cadastramento/init_script.py
+++ servico-cadastramento/init_script.py
@@ -1,5 +1,5 @@
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pymongo
 
@@ -28,7 +28,7 @@ people_names = [
 for i in range(10):
     clientes.append(
         {
-            "codigo": random.randint(10000, 99999),
+            "codigo": i + 1,
             "nome": people_names[i],
             "email": f"{people_names[i].lower()}@email.com",
         }
@@ -43,7 +43,7 @@ app_names = ["QuickQuest", "FitFleet", "MindMerge", "ChoreChampion", "TravelTrov
 for i in range(5):
     aplicativos.append(
         {
-            "codigo": random.randint(10000, 99999),
+            "codigo": i + 1,
             "nome": app_names[i],
             "custo_mensal": random.uniform(10.0, 25.0),
         }
@@ -53,15 +53,24 @@ for i in range(5):
 # pelo menos 5 assinaturas diferentes
 collection = db["assinaturas"]
 collection.create_index([("codigo", pymongo.ASCENDING)], unique=True)
+collection.create_index([("cod_aplicativo", pymongo.ASCENDING)], unique=False)
+collection.create_index([("cod_cliente", pymongo.ASCENDING)], unique=False)
+
+start_date = datetime.now()
+end_date = datetime(2025, 12, 31)
+
+
 assinaturas = []
 for i in range(5):
+    random_seconds = random.randint(0, int((end_date - start_date).total_seconds()))
+    random_datetime = start_date + timedelta(seconds=random_seconds)
     assinaturas.append(
         {
-            "codigo": random.randint(10000, 99999),
+            "codigo": i + 1,
             "cod_aplicativo": aplicativos[i]["codigo"],
             "cod_cliente": clientes[i]["codigo"],
             "inicio_vigencia": datetime.now(),
-            "fim_vigencia": datetime.now(),
+            "fim_vigencia": random_datetime,
         }
     )
     collection.insert_one(assinaturas[i])
